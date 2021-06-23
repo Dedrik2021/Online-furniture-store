0c637d1f6f7cb57c36e5fbfaaf2c47f322e5c468
Dedrik2021 <81495947+Dedrik2021@users.noreply.github.com>
2021-06-24 00:02:16 +0200

    added advantages, products, smart

--- app/js/main.js
+++ app/js/main.js
@@ -1,3 +1,26 @@
 $(function() {
+    $('.smart__list').slick({
+        fade: true,
+        arrows: false,
+        autoplay: true,
+        dots: true,
+    });
 
+    $('.search__btn').on('click', function() {
+        $('.search__input, .search__btn').toggleClass('active');
+    })
+
+    $('.user__link--heart').on('click', function() {
+        $('.user__link--heart').toggleClass('active');
+    })
+
+    $('.user__link--basket').on('click', function() {
+        $('.user__link--basket').toggleClass('active');
+    })
+
+    $('.user__more').on('click', function() {
+        $('.user__more').toggleClass('active');
+    })
 });
+
+const mixer = mixitup('.gallery');
