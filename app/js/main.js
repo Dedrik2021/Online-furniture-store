19ed502fd24c0618dc36bbcf04cb31e15e34c1f5
Dedrik2021 <81495947+Dedrik2021@users.noreply.github.com>
2021-06-24 22:56:55 +0200

    added styles

--- app/js/main.js
+++ app/js/main.js
@@ -1,10 +1,4 @@
 $(function() {
-    $('.smart__list').slick({
-        fade: true,
-        arrows: false,
-        autoplay: true,
-        dots: true,
-    });
 
     $('.search__btn').on('click', function() {
         $('.search__input, .search__btn').toggleClass('active');
@@ -21,6 +15,28 @@ $(function() {
     $('.user__more').on('click', function() {
         $('.user__more').toggleClass('active');
     })
+
+    // $(document).mouseup(function (e) {
+    //     var container = $('.search__btn.active, .search__input.active');
+    //     if (container.has(e.target).length === 0){
+    //         container.hide();
+    //     }
+    // });
+
+    $(window).scroll(function() {
+        if ($(this).scrollTop() > 1){
+        $('.header').addClass('sticky');
+        } else {
+            $('.header').removeClass('sticky');
+        }
+    });
+
+    $('.smart__list').slick({
+        fade: true,
+        arrows: false,
+        autoplay: true,
+        dots: true,
+    });
 });
 
 const mixer = mixitup('.gallery');
