4c84f42024501b570b66dd33f2e76a8fa1ad001d
Dedrik2021 <81495947+Dedrik2021@users.noreply.github.com>
2021-08-13 19:41:37 +0200

    added product-list1, product-list-2, adaptive home-page

--- app/js/main.js
+++ app/js/main.js
@@ -16,12 +16,12 @@ $(function() {
 
     $('.menu__link--page').on('click', function(e) {
         e.preventDefault();
-        $('.menu-dropdown--page').toggleClass('_active');
+        $('.menu__item--page, .menu-dropdown--page, .menu__link--page').toggleClass('_active');
     })
 
     $('.menu__link--home').on('click', function(e) {
         e.preventDefault();
-        $('.menu-dropdown--home').toggleClass('_active');
+        $('.menu__item--home, .menu-dropdown--home, .menu__link--home').toggleClass('_active');
     })
 
     $('.user-nav__more').on('click', function(e) {
@@ -35,7 +35,8 @@ $(function() {
         $('.menu__list').toggleClass('_active');
         $('.menu__burger').toggleClass('_active');
         $('.user-nav__search').toggleClass('_active');
-        $('.menu__logo').toggleClass('_active');
+        $('.user-nav').toggleClass('_active');
+        $('.lock').toggleClass('_active');
     })
     
     $(document).mouseup(function (e) {
@@ -46,9 +47,9 @@ $(function() {
     });
 
     $(document).mouseup(function (e) {
-        var container = $('.user-nav');
-        if (container.has(e.target).length === 0 && $('.user-nav__input, .user-nav__btn, .user-nav__search').hasClass('active')) {
-            $('.user-nav__input, .user-nav__btn, .user-nav__search').removeClass('active');
+        var container = $('.menu__list');
+        if (container.has(e.target).length === 0 && $('.menu__item--page, .menu__item--home, .menu-dropdown--home, .menu-dropdown--page, .menu__link--page, .menu__link--home').hasClass('_active')) {
+            $('.menu__item--page, .menu__item--home, .menu-dropdown--home, .menu-dropdown--page, .menu__link--page, .menu__link--home').removeClass('_active');
         }
     });
 
@@ -101,8 +102,7 @@ $(function() {
         $('.products-filters').slick({
             prevArrow: '<button type="button" class="slick-btn slick-prev"><<<</button>',
             nextArrow: '<button type="button" class="slick-btn slick-next">>>></button>',
-            infinite: true,
-            autoplay: 3000
+            slidesToScroll: 1,
         });
         isDestroyed = false;
     }
@@ -123,6 +123,56 @@ $(function() {
         else if (!isMobile && !isDestroyed) destroySlick();
     });
 
+    $(".filters-price__input").ionRangeSlider({
+        type: 'double',
+        min: 0.00,
+        max: 999.99,
+        from: 100.01,
+        to: 500.01,
+        step: 0.01,
+
+        onStart: function (data) {
+            $('.filters-price__from').text(data.from);
+            $('.filters-price__to').text(data.to);
+        },
+
+        onChange: function (data) {
+            $('.filters-price__from').text(data.from);
+            $('.filters-price__to').text(data.to);
+        }
+    });
+
+    $('.filters-product__select').styler();
+
+    $(".recent__stars").rateYo({
+        rating: 3.6,
+        starWidth: '12px',
+        normalFill: '#d6d6d6',
+        ratedFill: '#ffcc00',
+        spacing   : "6px",
+        rating: "80%",
+    });
+
+    $(".product-card__stars").rateYo({
+        rating: 3.6,
+        starWidth: '20px',
+        normalFill: '#d6d6d6',
+        ratedFill: '#ffcc00',
+        spacing   : "11px",
+        rating: "80%",
+    });
+
+    // $('.product-card__btn--add').on('click', function() {
+    //     $('.product-card__add, .product-card__list').toggleClass('active');
+        
+    // })
+
+    // $(document).mouseup(function (e) {
+    //     var container = $('.product-card__buttons');
+    //     if (container.has(e.target).length === 0 && $('.product-card__add, .product-card__list').hasClass('active')) {
+    //         $('.product-card__add, .product-card__list').removeClass('active');
+    //     }
+    // });
 });
 
 var containerEl1 = document.querySelector('.products-week__content');
@@ -136,8 +186,28 @@ var config = {
 
 var mixer1 = mixitup(containerEl1, config);
 var mixer2 = mixitup(containerEl2, config);
+var mixer3 = mixitup(containerEl3, config);
 
 const el = document.querySelector('button.user-nav__btn');
 console.log('before -> ', el);
 el.setAttribute('type', 'reset');
 console.log('after -> ', el);
+
+
+// const cards = document.querySelectorAll('.filters-product');
+
+// cards.forEach((card)=>{
+//     card.querySelector('select').addEventListener('click', () => {
+//         card.querySelector('.jq-selectbox__trigger-arrow').classList.toggle('active');
+//     });
+// })
+
+
+// const cards = document.querySelectorAll('.product-card__btn');
+
+// cards.forEach((card)=>{
+//     card.querySelector('select').addEventListener('click', () => {
+//         card.querySelector('.product-card__add, .product-card__list').classList.toggle('active');
+//     });
+// })
+
